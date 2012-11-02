85fd8db83a92428968bea25297cbe47c6f57f519
jatu <jari.tuikka@gmail.com>
2012-11-02 17:48:48 +0200

    adding new layouts

---
+++ src/school/exercise/geotrack/SplashActivity.java
@@ -0,0 +1,34 @@
+package school.exercise.geotrack;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        
+        TimerTask task = new TimerTask() {
+        	public void run() {
+        		Intent mainIntent = new Intent().setClass(SplashActivity.this, TabLayoutActivity.class);
+        		startActivity(mainIntent);
+        	}
+        };
+        
+        Timer timer = new Timer();
+        timer.schedule(task, 5000);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_splash, menu);
+        return true;
+    }
+}

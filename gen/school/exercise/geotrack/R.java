6465887c554e00ec6b111381cb9ed33d0abf0abe
jatu <jari.tuikka@gmail.com>
2012-11-02 17:46:18 +0200

    adding tabs

--- gen/school/exercise/geotrack/R.java
+++ gen/school/exercise/geotrack/R.java
@@ -13,6 +13,9 @@ public final class R {
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int icon_list_tab=0x7f020002;
+        public static final int icon_main_tab=0x7f020003;
+        public static final int icon_map_tab=0x7f020004;
     }
     public static final class id {
         public static final int icon=0x7f070000;
@@ -25,12 +28,14 @@ public final class R {
         public static final int activity_main=0x7f030001;
         public static final int activity_map_view=0x7f030002;
         public static final int activity_splash=0x7f030003;
+        public static final int activity_tab_layout=0x7f030004;
     }
     public static final class menu {
         public static final int activity_list_view=0x7f060000;
         public static final int activity_main=0x7f060001;
         public static final int activity_map_view=0x7f060002;
         public static final int activity_splash=0x7f060003;
+        public static final int activity_tab_layout=0x7f060004;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
@@ -41,6 +46,7 @@ public final class R {
         public static final int title_activity_main=0x7f040003;
         public static final int title_activity_map_view=0x7f040004;
         public static final int title_activity_splash=0x7f040006;
+        public static final int title_activity_tab_layout=0x7f040008;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;

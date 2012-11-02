5159e7b196ad1a4766b5080f349a8c80d509a9f2
Risord <niko.elmari@gmail.com>
2012-11-02 15:28:16 +0200

    päivitys2

--- src/school/exercise/geotrack/MainActivity.java
+++ src/school/exercise/geotrack/MainActivity.java
@@ -2,6 +2,7 @@ package school.exercise.geotrack;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,9 @@ public class MainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Intent i = new Intent(this, MapView.class);        
+        startActivity(i);
     }
 
     @Override
--- src/school/exercise/geotrack/MapView.java
+++ src/school/exercise/geotrack/MapView.java
@@ -1,10 +1,12 @@
 package school.exercise.geotrack;
 
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import com.google.android.maps.MapActivity;
+//import android.app.Activity;
 
-public class MapView extends Activity {
+
+public class MapView extends MapActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -17,4 +19,11 @@ public class MapView extends Activity {
         getMenuInflater().inflate(R.menu.activity_map_view, menu);
         return true;
     }
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
 }
